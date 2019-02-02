1f69602a6257653df5e95d26ca692805ceb9d820
Emilykradke <emilykradke@gmail.com>
2019-02-02 03:02:48 -0600

    comment

--- assets/javascript/yelp.js
+++ assets/javascript/yelp.js
@@ -8,144 +8,85 @@ var hoursOpen;
 var openArray = [];
 var hoursClosed;
 var closedArray = [];
-var days = ["Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"]
 var index = 0;
-var restaurants;
-// create div for all of the  restaurant information
-var restaurantDiv = $("<div>");
+var restaurantIndex = 0;
+var restaurants = [];
 
-var createRestCard = function(restaurants) {
+// hide the restaurant container on load of the page 
+$('.restaurant-container').hide();
 
-    var queryURLyelpRest = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + restaurants.id
-        $.ajax({
-            url: queryURLyelpRest,
-            method: "GET",
-            dataType: "json",
-            headers: {
-                "Authorization": "Bearer pTQUykRWiS1bm2vmyys495t9RcKg-SVvv72vC734JyFxrQEaveyZPQCKAMbg5MSMUCFaP13aBAtXoFtRY_xqXi3tCX5bOWsU-a4YiSMvywpYG9softopDf5O5XxOXHYx"
-            }
-        }).then(function(response) {
-            console.log(response);
-            
-            console.log(response.location.display_address[0])
-            name = response.name;
-            address = `${response.location.display_address[0]}
-            ${response.location.display_address[1]}`
-            photo1 = response.photos[0];
-            photo2 = response.photos[1];
-            photo3 = response.photos[2];
-            rating = response.rating;
-            price = response.price;
-            phone = response.display_phone;
+// function to create the restaurant information from the ajax response
+var createRestCard = function(restaurant) {
+    $('.restaurant-container').show();
 
-            for(var i = 0; i < 7; i++) {
-                hoursOpen = response.hours[0].open[i].start;
-                var openCon = moment(hoursOpen, 'HHmm').format('LT')
-                console.log(openCon)
-                openArray.push(openCon);
-                console.log(hoursOpen);
-                hoursClosed = response.hours[0].open[i].end;
-                var closedCon = moment(hoursClosed, 'HHmm').format('LT')
-                closedArray.push(closedCon);
-                console.log(hoursClosed);
-            }
+    var queryURLyelpRest = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/" + restaurant.id;
 
-            for(var i = 0; i < 7; i++) {
-                var pHours = $("<p>").text(`${days[i]}: ${openArray[i]} - ${closedArray[i]}`);
-                console.log(pHours);
+    $.ajax({
+        url: queryURLyelpRest,
+        method: "GET",
+        dataType: "json",
+        headers: {
+            "Authorization": "Bearer pTQUykRWiS1bm2vmyys495t9RcKg-SVvv72vC734JyFxrQEaveyZPQCKAMbg5MSMUCFaP13aBAtXoFtRY_xqXi3tCX5bOWsU-a4YiSMvywpYG9softopDf5O5XxOXHYx"
+        }
+    }).then(function(response) {
+        name = response.name;
+        address = `${response.location.display_address[0]}
+        ${response.location.display_address[1]}`
+        photo1 = response.photos[0];
+        rating = response.rating;
+        price = response.price;
+        phone = response.display_phone;
 
-                var hoursDiv = $("<div>");
-                // append hoursDiv to the restaurantDiv
-                hoursDiv.append(pHours);
-                restaurantDiv.append(hoursDiv);
-                
-            }
-            
-            
-            
-            
-            // create an image div for the restaurant images to go to and img divs for each photo
-            var photoDiv = $("<div>");
-            // give photoDiv a class
-            photoDiv.addClass("restaurantPhotos");
-            
-            var imgDiv1 = $("<img>");
-            var imgDiv2 = $("<img>");
-            var imgDiv3 = $("<img>");
-                
-            // give photoDiv attributes for image
-            imgDiv1.attr("src", photo1);
-            imgDiv1.addClass("slickImg")
-            imgDiv2.attr("src", photo2);
-            imgDiv2.addClass("slickImg")
-            imgDiv3.attr("src", photo3);
-            imgDiv3.addClass("slickImg")
-            // put the name variabe inside of a p tag
-            var pName = $("<p>").text(name);
+        // clear hour arrays and repopulate with restaurant hours
+        openArray = [];
+        closedArray = [];
+        console.log(response);
 
-            // put the rating into a p tag
-            var pRating = $("<p>").text(`Rating: ${rating}/5`);
+        // Getting the time from the ajax response, converting it from military time, and then pushing it to an array
+        for(var i = 0; i < 7; i++) {
+            hoursOpen = response.hours[0].open[i].start;
+            var openCon = moment(hoursOpen, 'HHmm').format('LT');
+            openArray.push(openCon);
+    
+            hoursClosed = response.hours[0].open[i].end;
+            var closedCon = moment(hoursClosed, 'HHmm').format('LT');
+            closedArray.push(closedCon);
+        }
 
-            // put the address into a p tag
-            var pAddress = $("<p>").text(address);
-            
-            // put the phone number into a p tag
-            var pPhone = $("<p>").text(phone);
-            
-            // put the price into a p tag
-            var pPrice = $("<p>").text(`Price: ${price}`);
-            
-            // put all of the divs into the restaurantDiv
-            restaurantDiv.prepend(pRating);
-            restaurantDiv.prepend(pAddress);
-            restaurantDiv.prepend(pPhone);
-            restaurantDiv.prepend(pPrice);
-            restaurantDiv.prepend(pName);
-            
-            // append each image into the photoDiv
-            photoDiv.append(imgDiv1)
-            photoDiv.append(imgDiv2)
-            photoDiv.append(imgDiv3)
-            
-            
+        // populate hours in restaurant card
+        for(var i = 0; i < 7; i++) {
+            $(`#hours-${i}`).text(`${openArray[i]} - ${closedArray[i]}`);
+        }
 
-            // append the photoDiv into the restaurantDiv
-            restaurantDiv.prepend(photoDiv);
-            
-            // put the restaurantDiv into the restaurant-container div from the html
-            $("#restaurant-container").prepend(restaurantDiv);
-        })
-    }
-
+        // populating the restaurant card DOM elements
+        $("#restaurant-title").text(name);
+        $("#restaurant-price").text(price);
+        $("#restaurant-phone").text(phone);
+        $("#restaurant-address").text(address);
+        $("#restaurant-rating").text(`${rating}/5`);
+        $("#restaurant-photo").attr("src", photo1);
+    })
+}
 
 $(document.body).on("click", "#search", function() {
     event.preventDefault();
 
     var zipCode = $("#zipCode").val().trim();
     
-    var queryURL = "http://dataservice.accuweather.com/locations/v1/search?q=" + zipCode + "&apikey=93IHD7yLyLQ38YB3mDKE2xSQfPg20nHA"
+    var queryURL = "http://dataservice.accuweather.com/locations/v1/search?q=" + zipCode + "&apikey=93IHD7yLyLQ38YB3mDKE2xSQfPg20nHA";
    
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log(response)
+        // console.log(response);
         var latitude = response[0].GeoPosition.Latitude;
-
-        console.log(latitude)
         var longitude = response[0].GeoPosition.Longitude;
-        console.log(longitude)
-    
         var miles = $("#radiusMiles").val();
         var meters = miles * 1609;
-        console.log(meters);
-        
         var price = $("#price").val();
-        
         var searchTerm = $("#search-word").val().trim();
-
-        var queryURLyelp = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=" + latitude + "&longitude=" + longitude+ "&term=" + searchTerm + "&price=" + price + "&radius=" + meters;
-        console.log(queryURLyelp)
+        var queryURLyelp = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=" + latitude + "&longitude=" + longitude + "&term=" + searchTerm + "&price=" + price + "&radius=" + meters;
         return $.ajax({
             url: queryURLyelp, 
             method: "GET",
@@ -155,36 +96,51 @@ $(document.body).on("click", "#search", function() {
             },
         }).then(function(response){
             restaurants = response.businesses;
-            console.log(restaurants[index])
-            createRestCard(restaurants[index])
         })
-    }) 
-        
-            // for(var i = 0; i < 3; i++) {
-            //     console.log(i)
-            //     console.log(response.businesses[i])
-            //     restaurantsID = response.businesses[i].id
-                    
-            //     console.log(restaurantsID)
-
-
-            $(document.body).on("click", "#nextRestaurant", function() {
-                $("#restaurant-container").empty();
-                index++;
-                console.log(restaurants[index])
-                createRestCard(restaurants[index])
-                // createRestCard(restaurants[index]);
-                //index =1
-            
-
+    }).then(function() {
+        shuffle(restaurants);
+        createRestCard(restaurants[index]);
     })
 })
 
-// $(document.body).on("click", "#nextRestaurant", function() {
-//     $("#restaurant-container").empty();
-//     index++;
-//     createRestCard(restaurants);
-//     //index =1
+$(document.body).on("click", "#nextRestaurant", function() {
+    // if the index is already at the end of array, set it to the start
+    if (restaurantIndex === 5) {
+        restaurantIndex = 0;
+    } else {
+        restaurantIndex++;
+    }
 
+    createRestCard(restaurants[restaurantIndex]);
+})
 
-// })
+$(document.body).on("click", "#prevRestaurant", function() {
+    // if the index is already at the start of array, set it to the end
+    if (restaurantIndex === 0) {
+        restaurantIndex = 5;
+    } else {
+        restaurantIndex--;
+    }
+
+    createRestCard(restaurants[restaurantIndex]);
+})
+
+// I found this on GitHub (Shuffles an array that is passed as an argument)
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
